794cc651f5a4f3390e035a2c8a45ca596f03c0d9
NEGIAO <1482918576@qq.com>
2026-01-12 14:30:26 +0800

    bug

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
-// import cesium from 'vite-plugin-cesium'; // 已移除，改用动态加载避免自动插入 index.html
+import cesium from 'vite-plugin-cesium'; // 已移除，改用动态加载避免自动插入 index.html
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -11,7 +11,7 @@ export default defineConfig({
   base: './',
   plugins: [
     vue(),
-    // cesium(), // 已移除，改用动态加载
+    cesium(), // 已移除，改用动态加载
     !isProduction && vueDevTools()
   ].filter(Boolean),
   resolve: {
