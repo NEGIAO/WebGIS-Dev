de7e03fe154516a616d8cb7e7e43fe17f0a56ba7
NEGIAO <1482918576@qq.com>
2026-03-25 18:16:09 +0800

    动态引入cesium，消除对性能的干扰

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
-import cesium from 'vite-plugin-cesium';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -11,7 +10,6 @@ export default defineConfig({
   base: './',
   plugins: [
     vue(),
-    cesium(),
     !isProduction && vueDevTools()
   ].filter(Boolean),
   resolve: {
